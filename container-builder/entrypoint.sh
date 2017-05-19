cc02a95c6ca3474a4c56fc855c7e50f361c4236d
Vic Iglesias <viglesias@google.com>
2017-05-19 14:09:11 -0700

    Create entrypoint.sh

---
+++ container-builder/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -xe
+
+SOURCE_TYPE=$1
+BUILD_TYPE=$2
+
+if [ "$SOURCE_TYPE" = 'git-repo' ]; then
+    git clone $GIT_REPO repo
+elif [ "$SOURCE_TYPE" = 'cloud-soure-repo' ]; then
+    gcloud source repos clone $REPO_NAME repo
+else
+    echo "Unknown source type: $SOURCE_TYPE"
+    exit 1
+fi
+
+pushd repo
+  if [ "$BUILD_TYPE" = 'container-builder' ]; then
+      gcloud container builds submit --tag $IMAGE_TAG .
+  elif [ "$BUILD_TYPE" = 'local-docker' ]; then
+      docker build -t $IMAGE_TAG .
+      docker push $IMAGE_TAG
+  else
+      echo "Unknown build type: $BUILD_TYPE"
+      exit 1
+  fi
+popd
