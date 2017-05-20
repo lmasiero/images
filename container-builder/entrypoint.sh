2928800f732bbcb6d888e6447d31642645281d4f
Vic Iglesias <viglesias@google.com>
2017-05-19 22:56:08 -0700

    Update entrypoint.sh

--- container-builder/entrypoint.sh
+++ container-builder/entrypoint.sh
@@ -20,7 +20,7 @@ fi
 
 pushd repo
   # TODO: Need to check if this tag exists
-  git checkout tags/$GIT_TAG
+  git checkout $GIT_REF
   if [ "$BUILD_TYPE" = 'container-builder' ]; then
       gcloud container builds submit --tag $IMAGE_TAG .
   elif [ "$BUILD_TYPE" = 'local-docker' ]; then
